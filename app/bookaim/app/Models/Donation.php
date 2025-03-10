e05a6ecfb974825ac6a931b0ad2d8b663cedab7b
Giorgia Cavalleri (Student) <N00230352@iadt.ie>
2025-03-10 12:25:35 +0000

    donation model

--- app/bookaim/app/Models/Donation.php
+++ app/bookaim/app/Models/Donation.php
@@ -2,9 +2,27 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Donation extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['user_id', 'community_id', 'notes'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    // public function community()
+    // {
+    //     return $this->belongsTo(Community::class);
+    // }
+
+    // public function books()
+    // {
+    //     return $this->hasMany(Book::class);
+    // }
 }
