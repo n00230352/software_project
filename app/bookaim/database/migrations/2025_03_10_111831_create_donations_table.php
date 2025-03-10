2595335b8a5e466a75413a6f73cdc10058f2bef8
Giorgia Cavalleri (Student) <N00230352@iadt.ie>
2025-03-10 11:59:35 +0000

    donation migration

---
+++ app/bookaim/database/migrations/2025_03_10_111831_create_donations_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('donations', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');  // User who donates the book
+            $table->foreignId('community_id')->constrained()->onDelete('cascade');  // Community that recives donation
+            $table->text('notes')->nullable();  // Optional notes about the donation
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('donations');
+    }
+};
